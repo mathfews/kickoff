994ad5afa1152415e1a5bb5fd9b48ef1fad2deb9
mathfews <matheusfcw1010@gmail.com>
2026-08-15 20:10:30 -0300

    feat: add simple scoreboard controls

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hi")
+const home_team_score = document.getElementById("home-team-score")
+const away_team_score = document.getElementById("away-team-score")
+const button_home = document.getElementById("button-home")
+const button_away = document.getElementById("button-away")
+
+let score_home = 2
+let score_away = 1
+
+
+home_team_score.textContent = score_home
+away_team_score.textContent = score_away
+
+
+button_home.addEventListener("click",() => {
+    home_team_score.textContent = ++score_home
+    console.log(home_team_score.textContent, away_team_score.textContent)
+})
+
+button_away.addEventListener("click",() => {
+    away_team_score.textContent = ++score_away
+    console.log(home_team_score.textContent, away_team_score.textContent)
+})
