e1004742026391453b94a120e4d7bae26cbee51b
mathfews <matheusfcw1010@gmail.com>
2026-08-15 19:26:25 -0300

    feat: serve frontend from server

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import { createServer } from "node:http"
+import fs from "node:fs/promises"
+import path from "node:path"
 
 const port = 8000
+const __dirname = import.meta.dirname
 
-const server = createServer((req, res) => {
-    res.write("kickoff")
-    res.end()
+const server = createServer( async (req, res) => {
+    res.setHeader("Content-type", "text/html")
+    const pathToIndexHTML = path.join(__dirname, "index.html")
+    const indexHTML = await fs.readFile(pathToIndexHTML, "utf8")
+    res.end(indexHTML)
 })
 
 server.listen(port, () => console.log(`running on: ${port}`))
